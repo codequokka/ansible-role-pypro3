6712752fd54615bca33030287f7e98ec54c21d29
Nobuchika Tanaka <codequokka@gmail.com>
2019-05-02 10:11:54 +0900

    Update pypro3 role to set ja locale

--- TODO.md
+++ TODO.md
@@ -1,2 +1,3 @@
 # TODOs
 - [ ] Examine how to let testinfra know ansible_python_interpreter
+- [ ] Examine how to suppress errors from a checking task initialy failed
--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,25 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_python_interpreter_exists(host):
+    f = host.file('/usr/bin/python')
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert f.linked_to == '/usr/bin/python3.5'
+
+
+def test_apt_packages_are_installed(host):
+    p = host.package('language-pack-ja')
+
+    assert p.is_installed is True
+
+
+def test_system_default_locale_is_ja(host):
+    f = host.file('/etc/default/locale')
+
+    assert f.contains('LANG=ja_JP.UTF-8') is True
+
+
+def test_user_locale_is_ja(host):
+    f = host.file('/home/vagrant/.bashrc')
+
+    assert f.contains('LANGUAGE=ja_JP:ja') is True
